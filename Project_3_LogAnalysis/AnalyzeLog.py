3c9e9d005444fa9ec9565361d9320b448fae1d0c
KanChen <kan@drchrono.com>
2017-10-10 14:56:29 -0700

    fix syntax

--- Project_3_LogAnalysis/AnalyzeLog.py
+++ Project_3_LogAnalysis/AnalyzeLog.py
@@ -1,11 +1,15 @@
 import psycopg2
 
+
 def problem1():
     # most popular article
     DB = psycopg2.connect("dbname=news")
     c = DB.cursor()
-    #article_reads is a view
-    c.execute("select title, views from article_reads, articles where article_reads.path like '/article/' || articles.slug order by views desc limit 3;")
+    # article_reads is a view
+    query = ("select title, views from article_reads, articles "
+             "where article_reads.path like '/article/' || articles.slug "
+             "order by views desc limit 3;")
+    c.execute(query)
     rows = c.fetchall()
     result = []
     for row in rows:
@@ -13,15 +17,16 @@ def problem1():
     DB.close()
     return result
 
+
 def problem2():
     # most popular author
     DB = psycopg2.connect("dbname=news")
     c = DB.cursor()
     query = ("select authors.name, sum(views) as t_views "
              "from articles, authors, article_reads "
-             "where articles.author = authors.id and path like '/article/' || articles.slug "
-             "group by authors.name order by t_views desc limit 3;"
-            )
+             "where articles.author = authors.id "
+             "and path like '/article/' || articles.slug "
+             "group by authors.name order by t_views desc limit 3;")
     c.execute(query)
     rows = c.fetchall()
     result = []
@@ -29,15 +34,15 @@ def problem2():
         result.append(row[0])
     return result
 
+
 def problem3():
     DB = psycopg2.connect("dbname=news")
     c = DB.cursor()
     query = ("select * from "
-             "(select date_request.time, (error_count * 100 /total ) as percent "
+             "(select date_request.time, (error_count*100/total) as percent "
              "from date_request join date_error "
              "on date_error.time = date_request.time) as temp "
-             "where percent > 1;"
-    )
+             "where percent > 1;")
     c.execute(query)
     rows = c.fetchall()
     result = rows[0][0]
